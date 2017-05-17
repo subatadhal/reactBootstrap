1bd453c9e666fe440aece450a7cd51507e109595
subata <subrata.dhal@gmail.com>
2017-05-17 09:21:14 +0530

    app created

---
+++ app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container" style={{'marginTop':'70px'}}>
+        <h1>Hi,</h1>
+        <h3>Mr. Unknown</h3>  
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var config = {
+   entry: './main.js',
+
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+
+   module: {
+      loaders: [
+        {
+              test: /\.css$/,
+              use: [ 'style-loader', 'css-loader' ]
+          },
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+
+   }
+}
+
+module.exports = config;
